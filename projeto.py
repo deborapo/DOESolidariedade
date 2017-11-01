166a9ff453f7719cf6d02c7d843c80e4f0120f1e
Debora Prado <pradodebora605@gmail.com>
2017-10-31 21:22:49 -0300

    criando rotas, pagina de cadastro

--- projeto.py
+++ projeto.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run()
