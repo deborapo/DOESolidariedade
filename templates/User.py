5394a646823f48dddf04f1057cd6361b744891b4
Maria <mariiawend123@gmail.com>
2017-11-10 21:35:05 -0200

    Logica do login

---
+++ templates/User.py
@@ -0,0 +1,21 @@
+from logica_do_login import users, login_manager
+import flask_login
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User.User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
